0839853de8edcc899217d0c4d17f45d5ce4afc5e
netomoura18@gmail.com <Neto Moura>
2023-12-12 02:53:02 -0300

    add enum APIKeys

---
+++ APIKeys.swift
@@ -0,0 +1,12 @@
+//
+//  APIKeys.swift
+//  MovieService
+//
+//  Created by Neto Moura on 12/12/23.
+//
+
+import Foundation
+
+enum APIKeys {
+    static let apiKey = "e0704089dab5a4884ecf67ab2aef73dd"
+}
