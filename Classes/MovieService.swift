fd546cb99c3e21eb6774b55f43b85be5f8be8d4c
netomoura18@gmail.com <Neto Moura>
2023-12-13 18:20:40 -0300

    add comments

--- Classes/MovieService.swift
+++ Classes/MovieService.swift
@@ -28,9 +28,11 @@ public class MovieService: MovieServiceProtocol {
     public func getMovies() -> AnyPublisher<MovieListModel, Error> {
         let urlString = "\(baseURL)\(popularMoviesURL)\(APIKeys.apiKey)"
         guard let url = URL(string: urlString) else {
+            // Retorna um publisher de erro se a URL for inválida
             return Fail(error: NSError(domain: "URL Inválida", code: 0, userInfo: nil)).eraseToAnyPublisher()
         }
 
+        // Executa a solicitação HTTP e decodifica a resposta em um modelo MovieListModel
         return URLSession.shared.dataTaskPublisher(for: url)
             .map(\.data)
             .decode(type: MovieListModel.self, decoder: JSONDecoder())
@@ -40,16 +42,19 @@ public class MovieService: MovieServiceProtocol {
     
     public func searchMovies(query: String) -> AnyPublisher<MovieListModel, Error> {
         guard !query.isEmpty else {
+            // Retorna um publisher de erro se a consulta estiver vazia
             return Fail(error: MovieServiceError.emptyQuery).eraseToAnyPublisher()
         }
 
         let urlString = "\(baseURL)\(searchURL)\(APIKeys.apiKey)&query=\(query)"
         
         guard let url = URL(string: urlString) else {
+            // Retorna um publisher de erro se a URL for inválida
             let error = MovieServiceError.invalidURL
             return Fail(error: error).eraseToAnyPublisher()
         }
         
+        // Executa a solicitação HTTP e decodifica a resposta em um modelo MovieListModel
         return URLSession.shared.dataTaskPublisher(for: url)
             .map(\.data)
             .decode(type: MovieListModel.self, decoder: JSONDecoder())
@@ -57,19 +62,24 @@ public class MovieService: MovieServiceProtocol {
     }
     
     public func loadImage(for movie: MovieListItem) -> AnyPublisher<UIImage?, Error> {
+        // Verifica se há um caminho de pôster e cria uma URL para a imagem
         guard let path = movie.posterPath,
               let url = URL(string: "\(imageBaseURL)\(path)") else {
+            // Retorna um publisher de imagem nula se o caminho do pôster for inválido
             return Just(nil)
                 .setFailureType(to: Error.self)
                 .eraseToAnyPublisher()
         }
 
+        // Tenta carregar a imagem do cache
         if let data = URLCache.shared.cachedResponse(for: URLRequest(url: url))?.data,
            let image = UIImage(data: data) {
+            // Retorna a imagem do cache se estiver disponível
             return Just(image)
                 .setFailureType(to: Error.self)
                 .eraseToAnyPublisher()
         } else {
+            // Se a imagem não estiver em cache, faz o download e armazena em cache
             return URLSession.shared.dataTaskPublisher(for: url)
                 .map(\.data)
                 .tryMap { data throws -> UIImage in
@@ -77,6 +87,7 @@ public class MovieService: MovieServiceProtocol {
                         throw MovieServiceError.invalidImageData
                     }
 
+                    // Armazena a imagem em cache
                     let response = URLResponse(url: url, mimeType: "image/jpeg", expectedContentLength: data.count, textEncodingName: nil)
                     let cachedResponse = CachedURLResponse(response: response, data: data)
                     URLCache.shared.storeCachedResponse(cachedResponse, for: URLRequest(url: url))
@@ -91,7 +102,7 @@ public class MovieService: MovieServiceProtocol {
     
 }
 
-
+// Enum para representar os possíveis erros relacionados ao MovieService
 enum MovieServiceError: Error {
     case invalidImageData
     case imageLoadingFailed
